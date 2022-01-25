b88e7de0830dcdef65de33f8e9e5003b932e1901
JHUnity <97708234+JHUnity@users.noreply.github.com>
2022-01-25 16:33:01 +0900

    2022-01-25

---
+++ BrainStorm(1)/Assets/Scripts/Unit/Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //점프
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("isJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+        //감속
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+        //스프라이트 반전
+        if (Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        if (Mathf.Abs(rigid.velocity.x) < 0.2)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+
+    }
+
+    void FixedUpdate()
+    {
+        //이동
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //최고속도
+        if (rigid.velocity.x > maxSpeed) //우방향 최고속도
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1)) //좌방향 최고속도
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        //착지 애니메이션 변경
+        if (rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0, 255));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    anim.SetBool("isJumping", false);
+                }
+            }
+        }
+        if ((Mathf.Abs(rigid.velocity.y) == 0) && anim.GetBool("isJumping"))
+        {
+            anim.SetBool("isJumping", false);
+        }
+    }
+}
--- BrainStorm(1)/Assets/Scripts/Unit/Unit.cs
+++ BrainStorm(1)/Assets/Scripts/Unit/Unit.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     public int hp;
-    public float movePower;
+    public float maxSpeed;
     public float jumpPower;
 
     public Rigidbody2D rigid;
+    public SpriteRenderer spriteRenderer;
+    public CapsuleCollider2D capsuleCollider;
+    public Animator anim;
     public Vector3 movement;
 
     void Awake()
     {
         rigid = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        anim = GetComponent<Animator>();
     }
 }
