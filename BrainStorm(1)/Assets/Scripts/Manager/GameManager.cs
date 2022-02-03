ae5cbd6faa0af505da42338eff82b36d59a8639e
JHUnity <97708234+JHUnity@users.noreply.github.com>
2022-02-03 16:43:41 +0900

    2022-02-03(16:43)

--- BrainStorm(1)/Assets/Scripts/Manager/GameManager.cs
+++ BrainStorm(1)/Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int star;
     public int bigStar;
 
-    public int maxStagePoint;
-    public int stagePoint;
+    public Player player;
+
+    public int maxStageScore;
+    public int stageScore;
     public int SitemNumber;
     public int stageIndex;
     public int savePoint;
     public float stageTime;
 
-
+    public Image[] UIhealth;
+    public Text UIStageScore;
 }
