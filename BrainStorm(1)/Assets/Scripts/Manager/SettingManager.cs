d5a21413a7cd789d07b0d90630e4d6dece05f6b1
JHUnity <97708234+JHUnity@users.noreply.github.com>
2022-03-03 19:39:21 +0900

    2022-03-03(19:39)

--- BrainStorm(1)/Assets/Scripts/Manager/SettingManager.cs
+++ BrainStorm(1)/Assets/Scripts/Manager/SettingManager.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingManager : MonoBehaviour
 {
     public GameManager gameManager;
+
+    public GameObject Menu;
+    public GameObject scoreSetting1;
+    public GameObject scoreSetting2;
+    public Slider bgmVolume;
+    public Slider seVolume;
+    public GameObject itemUI;
+
+    public bool Menuonoff;
+    public bool Itemonoff;
 
     public bool ScoreSetting;
     public float BGMSetting;
@@ -16,5 +27,56 @@ public class SettingManager : MonoBehaviour
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         //objManager = GameObject.Find("ObjectManager").GetComponent<ObjectManager>();
         //uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+
+        ScoreSet1();
+    }
+
+    void Update()
+    {
+        BGMSetting = bgmVolume.value * 100;
+        SESetting = seVolume.value * 100;
+    }
+
+    public void MenuButton()
+    {
+        if (Menuonoff == false)
+        {
+            Menu.SetActive(true);
+            Menuonoff = true;
+        }
+        else
+        {
+            Menu.SetActive(false);
+            Menuonoff = false;
+        }
+            
+    }
+
+    public void ScoreSet1()
+    {
+        scoreSetting1.SetActive(false);
+        scoreSetting2.SetActive(true);
+        ScoreSetting = false;
+    }
+
+    public void ScoreSet2()
+    {
+        scoreSetting1.SetActive(true);
+        scoreSetting2.SetActive(false);
+        ScoreSetting = true;
+    }
+
+    public void ItemSet()
+    {
+        if (Itemonoff == false)
+        {
+            itemUI.SetActive(true);
+            Itemonoff = true;
+        }
+        else
+        {
+            itemUI.SetActive(false);
+            Itemonoff = false;
+        }
     }
 }
